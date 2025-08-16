41dab181083ee54b6fbc722b62e3d641c4d63ce4
ankit-yadav90 <ankityadav73699@gmail.com>
2025-08-16 10:11:50 +0530

    Type of multipal function use

--- project4/script.js
+++ project4/script.js
@@ -3,3 +3,48 @@ function abcd(){
 }
 
 abcd();
+
+//return matlab jaha se aay ho wahi daal denge
+
+function abc() {
+  return 12;
+}
+
+let val = abc();
+console.log(val)
+
+//first class function ko valu ki trah  treat kare
+
+function abcde(val){
+    val();
+}
+
+abcde(function(){
+  console.log("hay")
+})
+
+//High order function wo function hota hei jo ki return kare function ya fir accept kare function apne parametr me
+
+function ab(){
+  return function(){
+    console.log("heyehye")
+  }
+}
+
+ab()();
+
+// closures ek functon jo return kre ek on function or return hone wala function youse karega parent function ka koi variable
+function a(){
+  let b = 12;
+  return function(){
+    console.log(b)
+  }
+}
+
+a()();
+
+//IIFE
+
+(function init(){
+  console.log("Initialized")
+})();
