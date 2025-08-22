989cc9fba63ee27000efe246670d6f799bd99bda
ankit-yadav90 <ankityadav73699@gmail.com>
2025-08-22 19:02:30 +0530

    use to the queryseletor and event lisntor

---
+++ practice10/script.js
@@ -0,0 +1,7 @@
+let h1 = document.querySelector("h1");
+h1.textContent = "update to the java script"
+
+h1.addEventListener("click", function(){
+  h1.style.color = "red"
+  h1.style.background = "green"
+});
