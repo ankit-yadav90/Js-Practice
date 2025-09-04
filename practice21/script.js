e9aa3a76cfe61dc76c00aeca5eb1a02b0aa5128f
ankit-yadav90 <ankityadav73699@gmail.com>
2025-09-04 21:10:04 +0530

    use to the bg changer

---
+++ practice21/script.js
@@ -0,0 +1,27 @@
+function SetDarkOrLight(){
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+}else {
+  document.body.classList.add("light");
+    document.body.classList.remove("dark");
+    
+}
+}
+SetDarkOrLight();
+
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", function(){
+  if(document.body.classList.contains("dark")){
+    document.body.classList.remove("dark");
+    document.body.classList.add("light");
+  }else{
+    document.body.classList.remove("light");
+    document.body.classList.add("dark");
+  }
+})
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", function(){
+  SetDarkOrLight();
+})
