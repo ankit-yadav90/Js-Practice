b7fd85a936be772dcc15c8febd38b4b0f5e287a4
ankit-yadav90 <ankityadav73699@gmail.com>
2025-09-17 12:01:51 +0530

    clicable button

---
+++ practice26/script.js
@@ -0,0 +1,6 @@
+let btn = document.getElementById("myButton");
+let classList = document.querySelector(".myClass");
+btn.addEventListener("click", function() {
+  classList.style.backgroundColor = "red";
+   
+});
