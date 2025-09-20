9487fa87b35ca692e3d6b9bb67f57a19b5708b2b
ankit-yadav90 <ankityadav73699@gmail.com>
2025-09-20 13:13:29 +0530

    oops concept use

---
+++ practice28/basics/script.js
@@ -0,0 +1,16 @@
+function CriatePancile(name, price, color, company){
+    this.name = name;
+    this.price = price;
+    this.color = color;
+    this.company = company;
+    
+}
+CriatePancile.prototype.write = function(text){
+      let h1 = document.createElement("h1");
+      h1.textContent = text;
+      h1.style.color = this.color;
+      document.body.appendChild(h1);
+    }
+
+let panscil1 = new CriatePancile("Natraj", 10, "red", "Natraj");
+let panscil2 = new CriatePancile("doms", 10, "black", "doms");
